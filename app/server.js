55770ce87bc6b6efb49733460dc388fb64282ad6
jimmyb6627 <brown.james.s@gmail.com>
2022-07-01 09:20:28 -0700

    fda

--- app/server.js
+++ app/server.js
@@ -9,3 +9,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+<p id="demo">"JavaScript can change HTML content."</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
